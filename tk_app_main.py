0841531e31fb3350aa9584393105ce28446f8fa5
Nôm Keyboard <nom-keyboard@proton.me>
2025-08-30 19:11:22 +0900

    add other widgets into window

--- tk_app_main.py
+++ tk_app_main.py
@@ -14,6 +14,7 @@ import tkinter.font
 import nomkb_alpha
 import nomkb_appres
 import nomkb_dict
+import nomkb_ui_tk
 
 TK_OVERRIDE_OLD_BEHAVIOR = 'break'
 TK_TEXT_START = '1.0'
@@ -49,6 +50,8 @@ except (csv.Error, UnicodeDecodeError, ValueError) as exc:
 (root := tkinter.Tk()).title('Nôm Keyboard')
 root.iconphoto(True, tkinter.PhotoImage(data=nomkb_appres.ICON_DATA))
 
+(status_label := tkinter.Label()).pack(fill=tkinter.X)
+
 (text_font := tkinter.font.Font(family='Nom Na Tong')).config(size=(default_font_size := text_font.actual()['size']))
 
 (text_area := tkinter.scrolledtext.ScrolledText(font=text_font, undo=True)).pack(expand=True, fill=tkinter.BOTH)
@@ -57,6 +60,11 @@ text_area.bind('<Control-equal>', change_text_size(2))
 text_area.bind('<Control-minus>', change_text_size(-2))
 text_area.bind('<Control-0>', change_text_size(default_font_size, relative=False))
 text_area.focus_set()
+
+(buffer_display := tkinter.Entry(state=tkinter.DISABLED)).pack(fill=tkinter.X)
+
+(list_view := nomkb_ui_tk.PagedListTk(root, 9)).tk_container.pack(fill=tkinter.X)
+list_view.tk_listbox.config(font=text_font)
 
 # make window size explicit
 root.update()
